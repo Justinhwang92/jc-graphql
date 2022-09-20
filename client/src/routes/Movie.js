349fb715717024bb7c803d9eededb4359bfec537
justinhwang92 <justinhwang92@gmail.com>
2022-09-20 14:42:52 -0400

    Add style and apollo local only field

--- client/src/routes/Movie.js
+++ client/src/routes/Movie.js
@@ -1,22 +1,92 @@
 import { gql, useQuery } from '@apollo/client';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 
 const GET_MOVIE = gql`
-  query getMovie($movieId: String!) {
+  query getMovie($movieId: ID!) {
     movie(id: $movieId) {
+      # remote fields
       id
       title
-      smaller_cover_image
+      medium_cover_image
+      rating
+      # local only field - only come from local cache
+      isLiked @client
+    }
   }
 `;
 
-export default function Movie() {
-  const { id } = useParams();
-  const { data, loading } = useQuery(GET_MOVIE, { variables: { movieId: id } });
-  console.log(data, loading);
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-  return <div>{data.movie.title}</div>;
+const Column = styled.div`
+  margin-left: 10px;
+  width: 50%;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Image = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  background-position: center center;
+  border-radius: 7px;
+`;
+
+export default function Movie() {
+  const { id } = useParams(); // get parameters from <Link> params
+  const {
+    data,
+    loading,
+    client: { cache },
+  } = useQuery(GET_MOVIE, {
+    // useQuery hook to get data from client
+    variables: {
+      movieId: id,
+    },
+  });
+  const onClick = () => {
+    // modify the apollo cache
+    cache.writeFragment({
+      id: `Movie:${id}`,
+      fragment: gql`
+        fragment MovieFragment on Movie {
+          isLiked
+        }
+      `,
+      data: {
+        isLiked: !data.movie.isLiked,
+      },
+    });
+  };
+  return (
+    <Container>
+      <Column>
+        <Title>{loading ? 'Loading...' : `${data.movie?.title}`}</Title>
+        <Subtitle>⭐️ {data?.movie?.rating}</Subtitle>
+        <button onClick={onClick}>
+          {data?.movie?.isLiked ? 'Unlike' : 'Like'}
+        </button>
+      </Column>
+      <Image bg={data?.movie?.medium_cover_image} />
+    </Container>
+  );
 }
